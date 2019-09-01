25fa03dfc0cb4cdf803b356cb74aa9dda8b7607a
vasiljevic <nebojsa.vasiljevic@gmail.com>
2019-09-01 23:16:55 +0200

    build updated

--- preview.bat
+++ preview.bat
@@ -1,4 +1,8 @@
-jupyter nbconvert --to html --template=petlja.tpl *.ipynb --output-dir docs
-type nul>docs\.nojekyll
+
+xcopy /I/Y/Q empty-docs docs
 xcopy /I/Y/Q pics docs\pics
 xcopy /I/Y/Q data docs\data
+jupyter nbconvert --to html --template=petlja.tpl *.ipynb --output-dir docs
+
+
+
